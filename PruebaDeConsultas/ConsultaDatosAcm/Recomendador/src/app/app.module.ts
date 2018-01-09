95c09aa9facfd39d980f9942fe0fada62de6fc33
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-01-09 15:15:43 -0500

    creacion componente inicio

--- PruebaDeConsultas/ConsultaDatosAcm/Recomendador/src/app/app.module.ts
+++ PruebaDeConsultas/ConsultaDatosAcm/Recomendador/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
