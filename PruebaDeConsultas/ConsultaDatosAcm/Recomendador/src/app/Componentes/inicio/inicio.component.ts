5c5c74b7b26cd2465098609f4f4d1d3913cf2adc
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-01-10 15:02:29 -0500

    consumo api acm
    se utilizo el endPoint http://acm.rkbexplorer.com/sparql/
    con la consulta por defecto, el resultado en Json, y la alternativa de
    HTTP GET

--- PruebaDeConsultas/ConsultaDatosAcm/Recomendador/src/app/Componentes/inicio/inicio.component.ts
+++ PruebaDeConsultas/ConsultaDatosAcm/Recomendador/src/app/Componentes/inicio/inicio.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {ArticuloInterface} from "../../Interfaces/articulo-interface";
+
+import {Http} from '@angular/http';
+import {ArticuloClass} from "../../Clases/articulo-class";
+
+
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  articulos: ArticuloInterface[] = [];
+
+  articulos2:  ArticuloClass[] = [];
+
+  constructor(private _http: Http) { }
 
   ngOnInit() {
+
+
+    this._http.get("http://acm.rkbexplorer.com/sparql/?format=json&query=PREFIX+id%3A+++%3Chttp%3A%2F%2Facm.rkbexplorer.com%2Fid%2F%3E%0D%0APREFIX+rdf%3A++%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0APREFIX+akt%3A++%3Chttp%3A%2F%2Fwww.aktors.org%2Fontology%2Fportal%23%3E%0D%0APREFIX+owl%3A++%3Chttp%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23%3E%0D%0APREFIX+akt%3A++%3Chttp%3A%2F%2Fwww.aktors.org%2Fontology%2Fportal%23%3E%0D%0APREFIX+akts%3A+%3Chttp%3A%2F%2Fwww.aktors.org%2Fontology%2Fsupport%23%3E%0D%0A%0D%0A%0D%0A%0D%0ASELECT+*+WHERE+%7B+%3Fs+rdfs%3Alabel+%3Fo+%7D+LIMIT+10%0D%0A%0D%0A")
+
+    //.map(response=>response.json()
+      .subscribe(
+        (response)=>{
+          console.log("Response:",response);
+
+          console.log(response.json());
+
+          let respuesta = response.json();
+
+          console.log(respuesta.next);
+
+          this.articulos = respuesta.results.bindings;
+
+
+        },
+        (error)=>{
+          console.log("Error:",error);
+        },
+        ()=>{
+          console.log("Finally");
+        }
+
+      )
+
+
   }
+
+
 
 }
--- PruebaDeConsultas/ConsultaDatosAcm/Recomendador/src/app/app.module.ts
+++ PruebaDeConsultas/ConsultaDatosAcm/Recomendador/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import {RoutesAppModules} from "./Routes";
 import { AppComponent } from './app.component';
 import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { ArticuloComponent } from './Componentes/articulo/articulo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    ArticuloComponent
   ],
   imports: [
     BrowserModule,
-    RoutesAppModules
+    RoutesAppModules,
+    FormsModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
