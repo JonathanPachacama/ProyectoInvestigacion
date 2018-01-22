156f3e72e3aeaa3d3b74ac9a0654bcb1a51bd43c
JonathanPachacama <jonathan.pachacama@epn.edu.ec>
2018-01-22 14:17:10 -0500

    modelo ArticuloACM

---
+++ PruebaDeConsultas/ConsultaDatosAcm/BackEnd/AppArticulo/api/controllers/ArticuloACMController.js
@@ -0,0 +1,11 @@
+/**
+ * ArticuloACMController
+ *
+ * @description :: Server-side logic for managing articuloacms
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
